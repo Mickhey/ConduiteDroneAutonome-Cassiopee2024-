2a0de9064b3c0eb2a742d7ef4ee8534692ec9b32
Nicolas Braga <nicolas.braga@telecom-sudparis.eu>
2024-03-29 22:09:59 +0100

    init

--- Autonomous-Drone-Driving-ADD--main/test.py
+++ Autonomous-Drone-Driving-ADD--main/test.py
@@ -4,7 +4,6 @@ import run_person_detector as detector
 import numpy as np
 import matplotlib.pyplot as plt
 
-t= []
 
 tello = Tello()
 tello.connect()
@@ -20,12 +19,12 @@ tello.move_up(60) # s'élève de 60cm en plus pour être a hauteur humaine
 
 
 for i in range (50): #boucle while meilleur mais flemme de traiter comment arreter la boucle
-    start = time.time()
-    cv2.imwrite("picture.png", frame_read.frame)
-    image = cv2.imread("picture.png")
+
+    image = frame_read.frame
+
     result, coordinates, barycenter = detector.run_object_detection(image) # Exécute la détection de personne sur l'image
     #print(coordinates, barycenter) # Affiche les coordonnées et le barycentre (optionnel, pratique pour debugage et tests)
-    if (coordinates != [0,0,0,0]):
+    if (coordinates != [0,0,0,0]): #si on détecte qqn
         rapport = (coordinates[1] - coordinates[0]) / (coordinates[3] - coordinates[2])
 
         bx = barycenter[0] # Coordonnée x du barycentre
@@ -34,35 +33,29 @@ for i in range (50): #boucle while meilleur mais flemme de traiter comment arret
         
 
                 # ==================== Algorithme de comportement du drone ===========
-        # ROTATION  
+        
         if(bx > 448):
-            print("rotate right")
+            #print("rotate right")
             tello.rotate_clockwise(10) 
         elif(bx < 408):
-            print("rotate left")
+            #print("rotate left")
             tello.rotate_counter_clockwise(10)  
-
+        
         # LIGNES SUIVANTES A TESTER 
         #elif (rapport >= 2 and by < 220):
         #    print("move frontward")
         #    tello.move_forward(20) 
-        #elif (by > 260):
-        #    print("move backward")
-        #    tello.move_back(20) 
+        elif (by > 260):
+            print("move backward")
+            tello.move_back(20) 
     
-    key = cv2.waitKey(1) # Ajoute un petit délai pour permettre à l'interface graphique de se mettre à jour
-    #cv2.imshow("Frame",image) si vous arrivez a faire marcher cv2.imshow
-    end = time.time()
-    t.append(end-start)
+    #key = cv2.waitKey(1) # Ajoute un petit délai pour permettre à l'interface graphique de se mettre à jour
+    #cv2.imshow("Drone tracking",image) si vous arrivez a faire marcher cv2.imshow
 
-print(t)
-print(np.mean(t))    
 
-#tello.flip_forward() #pour le lul
-
-time.sleep(5)
 
 tello.land()
-                
+tello.streamoff()
+
 tello.end()
                 
