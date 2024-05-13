ea7877629b9f3b8e3cb75ee712a6ccc0ee40fbeb
Clément Bourvic <clement.bourvic@telecom-sudparis.eu>
2024-05-13 17:00:21 +0200

    ajout de mesure sur yolov4

--- Autonomous-Drone-Driving-ADD--main/test.py
+++ Autonomous-Drone-Driving-ADD--main/test.py
@@ -4,61 +4,56 @@ import run_person_detector as detector
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Initialisation du drone
 tello = Tello()
 tello.connect()
-
 print(tello.get_battery())
 
 tello.streamon()
 frame_read = tello.get_frame_read()
 
 tello.takeoff()
+tello.move_up(60)  # s'élève de 60cm en plus pour être à hauteur humaine
 
-tello.move_up(60) # s'élève de 60cm en plus pour être a hauteur humaine
+# Initialisation des listes pour stocker les temps
+cycle_times = []
+inference_times = []
 
-
-for i in range (50): #boucle while meilleur mais flemme de traiter comment arreter la boucle
+for i in range(50):
     start = time.time()
     image = frame_read.frame
     temps_calcul_start = time.time()
-    result, coordinates, barycenter = detector.run_object_detection(image) # Exécute la détection de personne sur l'image
+    result, coordinates, barycenter = detector.run_object_detection(image)  # Exécute la détection de personne sur l'image
     temps_calcul_end = time.time()
-    #print(coordinates, barycenter) # Affiche les coordonnées et le barycentre (optionnel, pratique pour debugage et tests)
-    if (coordinates != [0,0,0,0]): #si on détecte qqn
-        rapport = (coordinates[1] - coordinates[0]) / (coordinates[3] - coordinates[2])
 
-        bx = barycenter[0] # Coordonnée x du barycentre
-        by = barycenter[1] # Coordonnée y du barycentre
-        
-        
+    # Enregistrer les temps
+    inference_time = temps_calcul_end - temps_calcul_start
+    inference_times.append(inference_time)
 
-                # ==================== Algorithme de comportement du drone ===========
-        
-        if(bx > 448):
-            #print("rotate right")
-            tello.rotate_clockwise(10) 
-        elif(bx < 408):
-            #print("rotate left")
-            tello.rotate_counter_clockwise(10)  
-        
-        # LIGNES SUIVANTES A TESTER 
-        #elif (rapport >= 2 and by < 220):
-        #    print("move frontward")
-        #    tello.move_forward(20) 
-        #elif (by > 260):
-        #    print("move backward")
-        #    tello.move_back(20) 
-    
-    #key = cv2.waitKey(1) # Ajoute un petit délai pour permettre à l'interface graphique de se mettre à jour
-    #cv2.imshow("Drone tracking",image) si vous arrivez a faire marcher cv2.imshow
+    if coordinates != [0, 0, 0, 0]:  # si on détecte quelqu'un
+        bx = barycenter[0]  # Coordonnée x du barycentre
+        by = barycenter[1]  # Coordonnée y du barycentre
+
+        if bx > 448:
+            tello.rotate_clockwise(10)
+        elif bx < 408:
+            tello.rotate_counter_clockwise(10)
+
     end = time.time()
-    print("i ème image",i)
-    print("delai en seconde",end-start)
-    print("temps d'inférence + calcul barycentre",temps_calcul_end-temps_calcul_start)
+    cycle_time = end - start
+    cycle_times.append(cycle_time)
+
+    print(f"{i + 1}ème image")
+    print("Délai par cycle en secondes:", cycle_time)
+    print("Temps d'inférence + calcul barycentre:", inference_time)
+
+# Calculer les moyennes des temps enregistrés
+average_cycle_time = sum(cycle_times) / len(cycle_times)
+average_inference_time = sum(inference_times) / len(inference_times)
+
+print("Temps moyen par cycle:", average_cycle_time)
+print("Temps moyen d'inférence:", average_inference_time)
 
 tello.land()
 tello.streamoff()
-
 tello.end()
-                
