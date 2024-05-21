759c14887d84e79ab33069549351b05b7c126182
SMark24 <mark.sarolian@telecom-sudparis.eu>
2024-05-21 23:43:11 +0200

    implémentation du pid rotation et avant/arrière

--- Autonomous-Drone-Driving-ADD--main/copy_yolov8.py
+++ Autonomous-Drone-Driving-ADD--main/copy_yolov8.py
@@ -3,8 +3,6 @@ import time
 import person_detector_yolov8 as detector
 import numpy as np
 from threading import Thread, Condition
-import matplotlib.pyplot as plt
-
 
 # Initialisation du drone
 tello = Tello()
@@ -15,27 +13,12 @@ tello.streamon()  # Activation du flux vidéo du drone
 frame_read = tello.get_frame_read()  # Récupération de l'objet de lecture des images
 
 tello.takeoff()  # Décollage du drone
-tello.move_up(100)
-
 stop_thread = False  # Variable pour contrôler l'arrêt des threads
 
 # Paramètres PID initiaux
-k_px = 0.03 # Gain proportionnel pour le contrôle PID optimal à 0.08 / 0.09 / 0.03
-k_dx = 0.09 # Gain dérivé pour le contrôle PID optimal à 0.06 / 0.09 / 0.09
-
-
-k_py = 0.1 # Gain proportionnel pour le contrôle PID optimal à 0.08 / 0.09 / 0.03
-k_dy = 0.1 # Gain dérivé pour le contrôle PID optimal à 0.06 / 0.09 / 0.09
-
-
+k_p = 0.1  # Gain proportionnel pour le contrôle PID
+k_d = 0.01  # Gain dérivé pour le contrôle PID
 previous_error_x = 0  # Erreur précédente sur l'axe X pour le contrôle PID
-previous_error_y = 0  # Erreur précédente sur l'axe X pour le contrôle PID
-x_err_sum = 0
-y_err_sum = 0
-
-xerrsl = []
-xerrl = []
-derrl = []
 
 # Dimensions de l'image du Tello EDU
 frame_width = 640  # Largeur de l'image du flux vidéo de base 1280 (natif sur le drone)
@@ -49,58 +32,24 @@ data_condition = Condition()  # Condition pour synchroniser les threads
 
 # Fonction pour commander le drone en fonction des résultats de détection
 def command_drone():
-    global previous_error_x,x_err_sum,x_error,previous_error_y,y_err_sum,y_error
+    global previous_error_x
     while not stop_thread:
         with data_condition:
             data_condition.wait(timeout=0.01)  # Attente d'une nouvelle détection ou d'un timeout
             if detection_results:
                 # Récupération des coordonnées barycentriques de la détection
                 bx, by = detection_results.pop(0)
-                rapport = (coordinates[1] - coordinates[0]) / (coordinates[3] - coordinates[2])
+                if(bx > 340):
+                    #print("rotate right")
+                    tello.rotate_clockwise(1) 
+                elif(bx < 300):
+                    #print("rotate left")
+                    tello.rotate_counter_clockwise(1)
                 
-                x_error = bx - frame_width // 2  # Calcul de l'erreur en X par rapport au centre de l'image
-                y_error = by - frame_height // 2
-
-
-                x_err_sum += x_error
-                dx_err = x_error - previous_error_x
-
-                y_err_sum += y_error
-                dy_err = y_error - previous_error_y
-
-
-                # Calcul de la commande de rotation PID
-                rotate_command = k_px * x_error - k_dx * abs(dx_err) 
-
-                pid_out_y = k_py * y_error - k_dy * abs(dy_err)
-                
-                # Normalisation de la commande pour qu'elle soit entre 0 et 360 degrés
-                rotate_command = max(0, min(360, int(abs(rotate_command))))
-
-                # Commande de rotation du drone
-                if x_error > 0:
-                    tello.rotate_clockwise(rotate_command)
-                elif x_error < 0:
-                    tello.rotate_counter_clockwise(rotate_command)
-                
-
-                if y_error > 0 and rapport >= 2:
-                    tello.move_forward(max(20,int(abs(pid_out_y))))
-                elif y_error < 0:
-                    tello.move_back(max(20,int(abs(pid_out_y))))
-
-                #xerrl.append(x_error)
-                #xerrsl.append(x_err_sum)
-                #derrl.append(d_err)
-                
-                previous_error_x = x_error  # Mise à jour de l'erreur précédente
-                previous_error_y = y_error
-
 
 # Fonction pour détecter les personnes à partir des images du drone
 def detection_thread():
     while not stop_thread:
-        global coordinates
         start_time = time.time()  # Temps de début du cycle
         image = frame_read.frame  # Récupération de l'image actuelle du flux vidéo
         inference_start = time.time()  # Temps de début de l'inférence
@@ -123,7 +72,7 @@ detection_thread.start()
 command_thread.start()
 
 try:
-    time.sleep(25)  # Temps de test (le drone fonctionne pendant 25 secondes à gerer autrement)
+    time.sleep(30)  # Temps de test (le drone fonctionne pendant 25 secondes à gerer autrement)
 finally:
     stop_thread = True  # Arrêt des threads
     with data_condition:
@@ -133,21 +82,8 @@ finally:
     tello.land()  # Atterrissage du drone
     tello.streamoff()  # Arrêt du flux vidéo
     print("Batterie:", tello.get_battery())  # Affichage du niveau de la batterie
-    print("x_err_sum =",x_err_sum)
-    print("x_error =",x_error)
-    print("previous error x =",previous_error_x)
-    #plt.plot(xerrl)
-    #plt.title("erreur courante")
-    #plt.show()
-    #plt.plot(xerrsl)
-    #plt.title("erreurs sommées")
-    #plt.show()
-    #plt.plot(derrl)
-    #plt.title("erreur dérivée")
-    #plt.show()
-
-
     tello.end()  # Déconnexion du drone
+
 
     # Calcul des moyennes des temps d'inférence et de cycle
     average_inference_time = np.mean(inference_times)
